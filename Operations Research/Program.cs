4304ce80ce723831b61e976b0351388aeccaa6b7
Ilya <villeman.5@yandex.ru>
2019-10-16 23:14:36 +0500

    Инициализация, скелет будущей программы

---
+++ Operations Research/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations_Research
+{
+    class Frac
+    {
+
+    }
+
+    class Operations
+    {
+        static public Frac[] dotProduct(Frac[] f1, Frac[] f2)
+        {
+            
+        }
+
+        static public Frac[] oneJordan(Frac[] matrix)
+        {
+
+        }
+
+        static public Frac[] fullJordan(Frac[] matrix)
+        {
+
+        }
+
+        static public Frac[] oneSimplex(Frac[] matrix, Frac[] target)
+        {
+
+        }
+
+        static public Frac[] fullSimplex(Frac[] matrix, Frac[] target)
+        {
+
+        }
+
+    }
+
+    class Utils
+    {
+        static void PrintMatrix(Frac[] matrix)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
